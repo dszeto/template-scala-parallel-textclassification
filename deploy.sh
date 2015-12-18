8fa6195fd492f0bb93376e1e8498a00912cff6bd
Donald Szeto <donald@prediction.io>
2015-12-17 23:40:56 -0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,12 @@ LAMBDA_HOME=/Users/dszeto/rcx/advance/backend/target/universal/stage
 ENGINE_INSTANCE_ID=`$LAMBDA_HOME/bin/lambda model log $1 | grep engineInstanceId | head -n 1 | cut -f2 -d= | tr -d '\n'`
 if [ ! -z "$ENGINE_INSTANCE_ID" ]; then
   $PIO_HOME/bin/pio deploy --engine-instance-id $ENGINE_INSTANCE_ID &
+  sleep 5
   disown
 else
   $PIO_HOME/bin/pio build
   $PIO_HOME/bin/pio train -- --driver-memory 4g
   $PIO_HOME/bin/pio deploy &
+  sleep 5
   disown
 fi
