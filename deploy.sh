74920d1bd0f1e79da8fc7d4ec553eff42c85a628
Donald Szeto <donald@prediction.io>
2016-01-01 16:17:45 -0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ PIO_HOME=/Users/dszeto/demo/PredictionIO-0.9.5
 PORT=8000
 LAMBDA_HOME=/Users/dszeto/rcx/advance/backend/target/universal/stage
 ENGINE_INSTANCE_ID=`$LAMBDA_HOME/bin/lambda model log $1 | grep engineInstanceId | head -n 1 | cut -f2 -d= | tr -d '\n'`
+echo "$1 => $ENGINE_INSTANCE_ID"
 if [ ! -z "$2" ]; then
   PORT=$2
 fi
